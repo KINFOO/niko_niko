d9b9f3a343ca066dd0cd93ad76ca79788ad216aa
Lionel Porcheron <lporcheron@sierrawireless.com>
2013-12-15 16:37:09 +0100

    Add final slash on voting URL also

--- niko_niko/urls.py
+++ niko_niko/urls.py
@@ -10,7 +10,7 @@ urlpatterns = patterns('',
     url(r'^polls/(?P<slug>[\w-]+)/$', 'niko.views.poll', name='poll'),
     url(r'^polls/(?P<slug>[\w-]+)/(?P<year>\d+)/(?P<month>\d+)/(?P<day>\d+)$',
         'niko.views.poll', name='poll_dated'),
-    url(r'^polls/(?P<slug>[\w-]+)/vote$', 'niko.views.vote', name='vote'),
+    url(r'^polls/(?P<slug>[\w-]+)/vote/$', 'niko.views.vote', name='vote'),
     url(r'^polls/(?P<slug>[\w-]+)/save/(?P<mood>\w+)$', 'niko.views.save', name='save'),
     # url(r'^niko_niko/', include('niko_niko.foo.urls')),
 
